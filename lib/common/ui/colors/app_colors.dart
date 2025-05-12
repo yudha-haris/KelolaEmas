582b5fd7d9ce9d14ed7d7cf6ab733bee28d61d14
yudha-haris <yudha.haris@ui.ac.id>
2025-05-12 19:37:27 +0700

    feat(ui): add import colors

---
+++ lib/common/ui/colors/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color success = Color(0xff34C759);
+
+  static Color goldPrimary = Color(0xffD4AF37);
+  static Color goldPrimaryDark = Color(0xffB58E2F);
+  static Color goldPrimaryLight = Color(0xffF8E6A0);
+
+  static Color textMuted = Color(0xff8E8E93);
+  static Color textPrimary = Color(0xff1C1C1E);
+  static Color textSecondary = Color(0xff3A3A3C);
+}
